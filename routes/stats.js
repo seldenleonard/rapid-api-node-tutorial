cd3d32e5f5a30ac66a5f16dc3f79ade669f33b48
seldenleonard <selden222@gmail.com>
2021-07-12 02:39:59 -0400

    creates post method

--- routes/stats.js
+++ routes/stats.js
@@ -52,3 +52,38 @@ router
   .post(createStats);
 
 module.exports = router;
+// PUT METHOD
+const updateStats = async (req, res, next) => {
+  try {
+    const data = fs.readFileSync(statsFilePath);
+    const stats = JSON.parse(data);
+    const playerStats = stats.find(player => player.id === Number(req.params.id));
+    if (!playerStats) {
+      const err = new Error('Player stats not found');
+      err.status = 404;
+      throw err;
+    }
+    const newStatsData = {
+      id: req.body.id,
+      wins: req.body.wins,
+      losses: req.body.losses,
+      points_scored: req.body.points_scored,
+    };
+    const newStats = stats.map(player => {
+      if (player.id === Number(req.params.id)) {
+        return newStatsData;
+      } else {
+        return player;
+      }
+    });
+    fs.writeFileSync(statsFilePath, JSON.stringify(newStats));
+    res.status(200).json(newStatsData);
+  } catch (e) {
+    next(e);
+  }
+};
+
+router
+  .route('/api/v1/stats/:id')
+  .get(getStats)
+  .put(updateStats);
